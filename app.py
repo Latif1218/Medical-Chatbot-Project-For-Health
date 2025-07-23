54ee9323fcaba52c15aacde5ca0297cbd6f65874
Md Abdul Latif Sumon <mdsabdullotif@gmail.com>
2025-07-24 04:35:24 +0600

    Web app completed

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+
+embeddings = download_embeddings()
+
+
+index_name = "medical-chatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3-Coder-480B-A35B-Instruct",  # Chat-compatible model
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    temperature=0.7,
+    max_new_tokens=512,
+    timeout=60
+)
+
+# Step 2: Pass it as `llm` into ChatHuggingFace
+chatModel = ChatHuggingFace(llm=llm)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ('human', "{input}")
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever,question_answer_chain)
+
+
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "you are an Medicale assistant for question-answering task."
+    "Use the following pieces of retrieved context to answer"
+    "the question. If you don't know the answer, say that you"
+    "don't know. Uae three sentences maximum and keep that"
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
